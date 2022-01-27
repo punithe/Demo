92531b966cb35c51b69498c656cde804d912528a
Punith E <punith.e7@gmail.com>
2022-01-27 16:40:46 +0530

    New line of code added

--- ConsoleA/ConsoleA/Program.cs
+++ ConsoleA/ConsoleA/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleA
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Sample line adding");
         }
     }
 }
